95cc424f9dbce77744aacdc40a5b5d6b101b90dd
Daniel <toomey.d.p@gmail.com>
2023-08-11 12:54:20 -0500

    First Commit
    HTML - Implement homepage scaffolding
    
    CSS - Apply grid layout with auto sized drawer column
    
    JS - Implement workaround for CSS transition effects being incompatible with computed styles. JS forces the browser to interpret computed drawer style width as actual width, thereby allowing for CSS transition on width to be rendered

---
+++ main.js
@@ -0,0 +1,35 @@
+let drawer = document.querySelector(".drawer");
+let dashIcon = document.querySelector(".dash-icon");
+let actualWidth = "";
+
+
+window.addEventListener("resize", setActualWidth);
+drawer.addEventListener("mouseover", addExtendoClass);
+drawer.addEventListener("mouseout", removeExtendoClass);
+
+    function setActualWidth() {
+        let CssObj = getComputedStyle(dashIcon);
+        actualWidth = CssObj.width;
+        drawer.style.width = actualWidth;
+        console.log("fired " + actualWidth);
+    }
+
+    function addExtendoClass() {
+        // drawer.style.width = actualWidth;
+        drawer.className += " extendo";
+    }
+
+    function removeExtendoClass() {
+        // location.reload();
+        // actualWidth = 20 + "px";
+        console.log("mouse out " + actualWidth);
+        // drawer.style.width = actualWidth;
+        drawer.classList.remove("extendo");
+    }
+
+setActualWidth();
+    
+
+
+
+
